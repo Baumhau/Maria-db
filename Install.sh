a857eb73a2d65ed85c86b5b3ea33bff978b659c9
Jawollo <139624810+Baumhau@users.noreply.github.com>
2025-01-27 19:13:03 +0100

    install.sh

---
+++ Install.sh
@@ -0,0 +1,3 @@
+wget https://downloads.mariadb.org/mariadb/repo/10.11/ubuntu/pool/main/m/mariadb-server/mariadb-server_10.11.8-1ubuntu22.04_amd64.deb
+sudo dpkg -i mariadb-server_10.11.8-1ubuntu22.04_amd64.deb
+sudo apt-get install -f
